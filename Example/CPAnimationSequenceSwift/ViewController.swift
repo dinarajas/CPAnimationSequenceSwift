cf21bdf3460ebf80b054e047ef7c9391880ebf8f
Dinesh Raja <dina.raja.s@gmail.com>
2015-10-29 22:38:19 +0530

    Example added

--- Example/CPAnimationSequenceSwift/ViewController.swift
+++ Example/CPAnimationSequenceSwift/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    @IBOutlet weak var button7: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        let animationSequence = CPAnimationSequence.sequenceWithSteps(
+            CPAnimationStep.animateFor(1.0, animation: { self.button1.alpha = 1.0 }),
+            CPAnimationStep.animateFor(0.8, animation: { self.button2.alpha = 1.0 }),
+            CPAnimationStep.animateFor(0.8, animation: { self.button3.alpha = 1.0 }),
+            CPAnimationStep.animateFor(0.8, animation: { self.button4.alpha = 1.0 }),
+            CPAnimationStep.animateFor(0.8, animation: { self.button5.alpha = 1.0 }),
+            CPAnimationStep.animateFor(0.8, animation: { self.button6.alpha = 1.0 }),
+            CPAnimationStep.animateFor(0.8, animation: { self.button7.alpha = 1.0 }))
+        animationSequence.run()
+    }
 }
 
